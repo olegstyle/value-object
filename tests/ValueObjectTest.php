13854dd9d8742538652b62f94931d0a01923d44a
Oleh Borysenko <olegstyle1@gmail.com>
2018-03-24 12:42:07 +0100

    added value object trait

--- tests/ValueObjectTest.php
+++ tests/ValueObjectTest.php
@@ -15,20 +15,20 @@ class ValueObjectTest extends TestCase
 {
     public function testObjectInObject()
     {
-        $b = B_ValueObject::fromArray([
+        $b = B_ValueObjectTrait::fromArray([
             'year' => 1994,
             'a' => [
                 'name' => 'Tester',
             ]
         ]);
 
-        $this->assertInstanceOf(A_ValueObject::class, $b->a);
+        $this->assertInstanceOf(A_ValueObjectTrait::class, $b->a);
         $this->assertEquals($b->a->name, 'Tester');
         $this->assertNull($b->a->surname);
     }
 }
 
-class A_ValueObject extends ValueObject
+class A_ValueObjectTrait extends ValueObject
 {
     /** @var string */
     public $name;
@@ -42,15 +42,15 @@ class A_ValueObject extends ValueObject
     }
 }
 
-class B_ValueObject extends ValueObject
+class B_ValueObjectTrait extends ValueObject
 {
     /** @var int */
     public $year;
 
-    /** @var A_ValueObject */
+    /** @var A_ValueObjectTrait */
     public $a;
 
-    public function __construct(int $year, A_ValueObject $a)
+    public function __construct(int $year, A_ValueObjectTrait $a)
     {
         $this->year = $year;
         $this->a = $a;
